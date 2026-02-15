8b602ea61739d6aaf21e860fe0c34dfacaf0cbe4
nengnai <403785045@qq.com>
2026-02-15 16:44:03 +0800

    123

--- Assets/Scripts/TowerPre1.cs
+++ Assets/Scripts/TowerPre1.cs
@@ -28,11 +28,13 @@ public class TowerPre1 : MonoBehaviour
             break;
 
             case 当前状态.预览:
-            进行预览();
+            预览中();
+            //投影持续跟随鼠标
                 if (Input.GetMouseButtonDown(1))
                 {
                     进入待机模式(true);
                 }
+                //右键取消投影
             break;
         }
     }
@@ -40,24 +42,34 @@ public class TowerPre1 : MonoBehaviour
     public void 选中塔(Toggle toggle, GameObject 预制体)
     {
         if (!toggle.isOn)
+        //点击“之后”按钮的状态
         {
             if(当前激活的Toggle == toggle)
+            //点击的按钮是上一次按的同一个按钮
             {
                 进入待机模式(false);
             }
             return;
         }
+        //当点击之后按钮是关闭状态执行并直接结束整段函数
+        //只有再次点击同一个按钮才能在检测的时候检测到关闭状态 所以理论上只要进入这个if 那么进入待机模式必定触发
 
         清理当前预览();
+        //清掉上一个投影
 
         if(当前激活的Toggle != null && 当前激活的Toggle != toggle)
+        //如果当前有一个按钮被选中 并且点击的按钮不是上一个被选中的按钮
         {
             当前激活的Toggle.isOn = false;
+            //关闭上一个被选中的按钮
         }
 
         当前激活的Toggle = toggle;
         进入预览(预制体);
         currentState = 当前状态.预览;
+        //把当前被选中的按钮更新成这次点击的按钮
+        //生成投影
+        //更新当前case状态
 
     }
 
@@ -65,20 +77,26 @@ public class TowerPre1 : MonoBehaviour
     {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
+        //射线检测 从摄像头射向鼠标位置
 
         if (Physics.Raycast(ray, out hit, 100f, 检测层级))
+        //Physics.Raycast(射线, 返回碰撞信息, 射线长度, 筛选层级)
         {
             当前预览 = Instantiate(预制体, hit.point, Quaternion.identity);
+            //Instantiate(生成的东西, 生成的位置, 方向)
+            //如果射线碰撞到对应层级 生成投影在鼠标位置
         }
         else
         {
             当前预览 = Instantiate(预制体, new Vector3(0,-999,0), Quaternion.identity);
+            //没碰到任何层级 生成在视野外某个地方
         }
     }
 
-    private void 进行预览()
+    private void 预览中()
     {
         if(当前预览 == null) return;
+        //如果没有需要生成投影 结束函数
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 100f, 检测层级))
@@ -95,12 +113,16 @@ public class TowerPre1 : MonoBehaviour
         清理当前预览();
 
         if(强制关闭UI && 当前激活的Toggle != null)
+        //如果当前有按钮被选中
         {
             当前激活的Toggle.isOn = false;
+            //关了按钮
         }
 
         当前激活的Toggle = null;
+        //清空状态
         currentState = 当前状态.待机;
+        //更新当前case状态
     }
 
     private void 清理当前预览()
