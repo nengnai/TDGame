a306f97502e7f13af876bf1798ca407ebcea8056
125690d <3617424629@qq.com>
2026-07-10 16:59:06 +0800

    -添加Subsystem模块，用于所有全局单例的父类 --WorldSubsystem用于当前场景的单例 --GameInstanceSubsystem用于全局单例 --实现静态获取方法 -整理了忽略文件，并从跟踪文件中移除了不需要的文件

---
+++ Assets/Scripts/Library/Subsystem.cs
@@ -0,0 +1,52 @@
+// 
+
+using System;
+using UnityEngine;
+
+public abstract class WorldSubsystem<T> : MonoBehaviour
+    where T : WorldSubsystem<T>
+{
+    protected static T _Subsystem;
+    
+    
+    protected virtual void Awake()
+    {
+        if (_Subsystem != null && _Subsystem != this)
+        {
+            Destroy(gameObject); // 防止重复实例
+            return;
+        }
+
+        // Subsystem本身是抽象的，所以直接强转
+        _Subsystem = this as T;
+    }
+
+    public static T GetSubsystem()
+    {
+        if (_Subsystem != null)
+        {
+            return _Subsystem;
+        }
+        
+        // @todo:这里也许以后会加一个开关？
+        if (true) 
+        {
+            GameObject NewObject = new GameObject();
+            _Subsystem = NewObject.AddComponent<T>();
+            return _Subsystem;
+        }
+        
+        return null;
+    }
+}
+
+
+public abstract class GameInstanceSubsystem<T> : WorldSubsystem<T>
+    where T : GameInstanceSubsystem<T>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(_Subsystem);
+    }
+}
