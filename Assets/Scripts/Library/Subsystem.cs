0002d48c23a0a7b0622450edce47db00f9cc0a44
125690d <3617424629@qq.com>
2026-07-10 17:22:45 +0800

    -修改了Subsystem所属的命名空间 -修改了新内容的初始声明

--- Assets/Scripts/Library/Subsystem.cs
+++ Assets/Scripts/Library/Subsystem.cs
@@ -3,50 +3,53 @@
 using System;
 using UnityEngine;
 
-public abstract class WorldSubsystem<T> : MonoBehaviour
-    where T : WorldSubsystem<T>
+namespace TDGameLibrary
 {
-    protected static T _Subsystem;
-    
-    
-    protected virtual void Awake()
+    public abstract class WorldSubsystem<T> : MonoBehaviour
+        where T : WorldSubsystem<T>
     {
-        if (_Subsystem != null && _Subsystem != this)
+        protected static T _Subsystem;
+
+
+        protected virtual void Awake()
         {
-            Destroy(gameObject); // 防止重复实例
-            return;
+            if (_Subsystem != null && _Subsystem != this)
+            {
+                Destroy(gameObject); // 防止重复实例
+                return;
+            }
+
+            // Subsystem本身是抽象的，所以直接强转
+            _Subsystem = this as T;
         }
 
-        // Subsystem本身是抽象的，所以直接强转
-        _Subsystem = this as T;
+        public static T GetSubsystem()
+        {
+            if (_Subsystem != null)
+            {
+                return _Subsystem;
+            }
+
+            // @todo:这里也许以后会加一个开关？
+            if (true)
+            {
+                GameObject NewObject = new GameObject();
+                _Subsystem = NewObject.AddComponent<T>();
+                return _Subsystem;
+            }
+
+            return null;
+        }
     }
 
-    public static T GetSubsystem()
-    {
-        if (_Subsystem != null)
-        {
-            return _Subsystem;
-        }
-        
-        // @todo:这里也许以后会加一个开关？
-        if (true) 
-        {
-            GameObject NewObject = new GameObject();
-            _Subsystem = NewObject.AddComponent<T>();
-            return _Subsystem;
-        }
-        
-        return null;
-    }
-}
 
-
-public abstract class GameInstanceSubsystem<T> : WorldSubsystem<T>
-    where T : GameInstanceSubsystem<T>
-{
-    protected override void Awake()
+    public abstract class GameInstanceSubsystem<T> : WorldSubsystem<T>
+        where T : GameInstanceSubsystem<T>
     {
-        base.Awake();
-        DontDestroyOnLoad(_Subsystem);
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(_Subsystem);
+        }
     }
 }
